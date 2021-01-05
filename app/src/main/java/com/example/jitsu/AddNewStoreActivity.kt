1f3775da2a56366a6cd10b50c66eac29f4c419f9
ShotaSuzuki <ja.ed.ha.13@gmail.com>
2021-01-05 16:28:54 +0900

    push

---
+++ app/src/main/java/com/example/jitsu/AddNewStoreActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jitsu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNewStoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_store)
+    }
+}
