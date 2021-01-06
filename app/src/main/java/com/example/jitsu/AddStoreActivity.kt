b0db4653d9976de55e61f0ad69d44f3670d36976
ShotaSuzuki <ja.ed.ha.13@gmail.com>
2021-01-06 17:19:43 +0900

    push

--- app/src/main/java/com/example/jitsu/AddStoreActivity.kt
+++ app/src/main/java/com/example/jitsu/AddStoreActivity.kt
@@ -1,11 +1,25 @@
 package com.example.jitsu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AddStoreActivity : AppCompatActivity() {
+    private val addActivityRequestCode = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_store)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@AddStoreActivity, AddNewStoreActivity::class.java)
+            startActivityForResult(intent, addActivityRequestCode)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
