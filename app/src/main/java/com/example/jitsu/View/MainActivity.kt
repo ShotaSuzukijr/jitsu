1aa3c27dec70de449bf925fee7da57206c3a1e7b
ShotaSuzuki <ja.ed.ha.13@gmail.com>
2021-02-01 09:29:36 +0900

    push

--- app/src/main/java/com/example/jitsu/View/MainActivity.kt
+++ app/src/main/java/com/example/jitsu/View/MainActivity.kt
@@ -6,9 +6,16 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jitsu.FoodListAdapter
 import com.example.jitsu.R
 
 class MainActivity : AppCompatActivity() {
+    private val mainActivityRequestCode = 2
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,6 +23,11 @@ class MainActivity : AppCompatActivity() {
         val SearchArea: Button = findViewById(R.id.SearchArea)
         val AddFoodMap: Button = findViewById(R.id.AddFoodMap)
         val TextArea: EditText = findViewById(R.id.editTextArea)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerviewMain)
+        val adapter = FoodListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
         SearchArea.setOnClickListener {
             val intent = Intent(this, SearchMapActivity::class.java)
             intent.putExtra("VALUE", TextArea.text.toString())
